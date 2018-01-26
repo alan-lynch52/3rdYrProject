a3599e97aa35307d08e9445c6d0f2b15a01cd1bd
Yggdrasil <jfc14cmu@uea.ac.uk>
2018-01-26 21:28:58 +0000

    progress random forest model

--- digit-recognizer/random-forest.py
+++ digit-recognizer/random-forest.py
@@ -5,24 +5,38 @@ import matplotlib.pyplot as plt
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from scipy.fftpack import dct
+def main():
+    train = pd.read_csv("train.csv")
+    #test = pd.read_csv("test.csv")
 
-train = pd.read_csv("train.csv")
-#test = pd.read_csv("test.csv")
+    y = train["label"]
+    x = train.drop(labels = ["label"], axis = 1)
+    
+    x = x.iloc[:,:]
+    #disp_img(y.iloc[1],x.iloc[1])
 
-y = train["label"]
-x = train.drop(labels = ["label"], axis = 1)
+    x = dct(x)
+    del train
 
-del train
+    random_seed = 2
+    x_train, x_validation, y_train, y_validation = train_test_split(x, y, test_size = 0.1, random_state = random_seed)
 
-print(y)
-
-print(x)
-random_seed = 2
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = random_seed)
-
-rf = RandomForestClassifier()
-rf.fit(x_train, y_train)
-
-print(rf.predict(x_test))
+    rf = RandomForestClassifier()
+    rf.fit(x_train, y_train)
 
 
+    print(rf.score(x_validation, y_validation))
+    #random forest base model: 93.97% accuracy
+    
+
+
+def disp_img(lbl, px):
+    img = px.as_matrix()
+    img = img.reshape((28,28))
+    plt.imshow(img, cmap='gray')
+    plt.title(lbl)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
