6262006bbd9dc8b08f26aec4dcc38cc7271f6879
Yggdrasil <jfc14cmu@uea.ac.uk>
2018-01-31 17:48:16 +0000

    merge

--- digit-recognizer/random-forest.py
+++ digit-recognizer/random-forest.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from scipy.fftpack import dct
+from sklearn.decomposition import pca
 def main():
     train = pd.read_csv("train.csv")
     #test = pd.read_csv("test.csv")
@@ -16,16 +17,19 @@ def main():
     x = x.iloc[:,:]
     #disp_img(y.iloc[1],x.iloc[1])
 
-    x = dct(x)
+    x_dct = dct(x)
+    x_pca = pca(n_components = 10)
+    x_pca.fit(x)
+    
     del train
 
     random_seed = 2
-    x_train, x_validation, y_train, y_validation = train_test_split(x, y, test_size = 0.1, random_state = random_seed)
+    x_train, x_validation, y_train, y_validation = train_test_split(x_pca, y, test_size = 0.1, random_state = random_seed)
 
     rf = RandomForestClassifier()
     rf.fit(x_train, y_train)
 
-
+    
     print(rf.score(x_validation, y_validation))
     #random forest base model: 93.97% accuracy
     
